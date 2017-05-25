2169b5f891fbfc0efa0e00540df882ee869a3eaa
William Makley <wmakley@tortus.com>
2017-05-24 20:36:16 -0400

    style improvements

--- deathgod/display.py
+++ deathgod/display.py
@@ -27,7 +27,11 @@ class Screen(View):
         self.__window = pygame.display.set_mode(settings.screen_dimensions)
         pygame.display.set_caption(settings.window_caption)
         screen = pygame.display.get_surface()
-        View.__init__(self, None, Rect(0, 0, screen.get_width(), screen.get_height()), colors.black, screen)
+        View.__init__(self,
+                      None,
+                      Rect(0, 0, screen.get_width(), screen.get_height()),
+                      colors.black,
+                      screen)
         event.add_handler(self.handle_display_needs_update, event.DisplayNeedsUpdate)
 
     @property
@@ -41,7 +45,7 @@ class Screen(View):
         View.paint(self)
         self.refresh()
 
-    def handle_display_needs_update(self, e):
+    def handle_display_needs_update(self, _):
         self.paint()
 
 
@@ -58,9 +62,9 @@ class Display(View):
         self.map_object = map_object
 
         View.__init__(self,
-                      parent = screen,
-                      rect = Rect(0, 0, screen.width, screen.height),
-                      clear_color = colors.black)
+                      parent=screen,
+                      rect=Rect(0, 0, screen.width, screen.height),
+                      clear_color=colors.black)
         # whoops, so much for the surface made by View.__init__
 
         # set up event handlers
@@ -71,7 +75,8 @@ class Display(View):
 
         self.set_clear_on_paint(True)
 
-        self.map_view = MapView(self,
+        self.map_view = MapView(
+            self,
             Rect(settings.map_view_position, settings.map_view_size),
             settings.map_center_color,
             settings.num_tiles_displayed
@@ -79,13 +84,15 @@ class Display(View):
 
         self.map_view.sprite_list = tile.get_tile_sprites(self.map_view.tile_size)
 
-        self.message_view = message.MessageView(self,
+        self.message_view = message.MessageView(
+            self,
             Rect(settings.message_view_position, settings.message_view_size),
             settings.message_view_bg_color,
             fonts.CourierNew(settings.message_text_size)
         )
 
-        self.status_view = StatusView(self,
+        self.status_view = StatusView(
+            self,
             Rect(settings.status_view_position, settings.status_view_size),
             settings.status_view_color,
             pygame.font.Font(settings.font_normal, settings.status_view_font_size),
@@ -128,9 +135,9 @@ class Display(View):
         self.parent.paint()
 
     #old
-    def handle_game_state_changed(self, e):
-        self.map_view.update(e.player_state, e.map_state)
-        self.status_view.update(e.player_state)
+    def handle_game_state_changed(self, evt):
+        self.map_view.update(evt.player_state, evt.map_state)
+        self.status_view.update(evt.player_state)
         self.parent.paint()
 
 
